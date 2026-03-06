740b672cdc6daff5832e18d0386d73817bdd9ee2
fidel <fidelvzcs@gmail.com>
2026-03-06 10:35:51 +0100

    ex001 Hello world printf some variables

---
+++ ex001/01operaciones.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() { 
+	char inicial;
+	int edad;
+	float temperatura = 23.5;
+
+	edad = 25;
+	inicial = 'F';	
+	printf("Hello world \n");
+	printf("Tu inicial es: %c\n", inicial);
+	printf("Tu edad es: %d\n", edad);
+	printf("La temperatura actual es: %f\n", temperatura);
+	return 0;
+} 
